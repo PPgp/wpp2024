de42bcc3f1d809c019cbb29786b5f6d6834c0534
Hana Sevcikova <hanas@uw.edu>
2024-11-16 16:20:11 -0800

    fixed order of aga groups in migrationX5

--- data/migrationM1.R
+++ data/migrationM1.R
@@ -1,9 +1,9 @@
 migrationM1 <- local({
     e <- new.env()
     load("migprojAge1dt.rda", envir= e)
+    data.table::setDT(e$migprojAge1dt)
     load("countries.rda", envir= e)
-    e$migprojAge1dt <- merge(data.table::as.data.table(e$migprojAge1dt), e$countries, 
-                             by = "country_code", sort = FALSE)
+    e$migprojAge1dt <- merge(e$migprojAge1dt, e$countries, by = "country_code", sort = FALSE)
     as.data.frame(data.table::dcast(e$migprojAge1dt, country_order + country_code + name + age ~ year, value.var = "migM")[, `:=`(country_order = NULL)][])
 })
 
--- data/migrationM5.R
+++ data/migrationM5.R
@@ -1,9 +1,11 @@
 migrationM5 <- local({
     e <- new.env()
     load("migprojAge5dt.rda", envir= e)
+    data.table::setDT(e$migprojAge5dt)
     load("countries.rda", envir= e)
-    e$migprojAge5dt <- merge(data.table::as.data.table(e$migprojAge5dt), e$countries, 
-                             by = "country_code", sort = FALSE)
-    as.data.frame(data.table::dcast(e$migprojAge5dt, country_order + country_code + name + age ~ year, value.var = "migM")[, `:=`(country_order = NULL)][])
+    e$migprojAge5dt <- merge(e$migprojAge5dt, e$countries, by = "country_code", sort = FALSE)
+    e$migprojAge5dt[, agecat := rep(1:21, nrow(e$migprojAge5dt)/21)]
+    as.data.frame(data.table::dcast(e$migprojAge5dt, country_order + country_code + name + agecat + age ~ year, 
+                                    value.var = "migM")[, `:=`(country_order = NULL, agecat = NULL)][])
 })
 
