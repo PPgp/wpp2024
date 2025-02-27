490f4091c7f63126cf1a17bdbab2437a4099c336
Hana Sevcikova <hanas@uw.edu>
2025-02-27 10:56:39 -0800

    comments

--- data-raw/create_pop5_data.R
+++ data-raw/create_pop5_data.R
@@ -1,3 +1,6 @@
+# Create a 5x5 version of the wpp2024 package to be used in wppExplorer
+# (if using 1x1 population it's very slow and crashes on shinyio)
+
 library(data.table)
 library(usethis)
 
@@ -5,38 +8,26 @@ library(usethis)
 setwd("~/bayespop/R/wpps/wpp2024/data-raw")
 wrkdir <- getwd()
 data.dir <- "../data" # data directory 
-
-#source("functions.R")
-
-# Population
-#############
-# 5-year age-specific pop estimates 
-# pop1 <- merge.with.age5(popAge1dt)
-# pop5 <- pop1[, .(popM = sum(popM), popF = sum(popF), pop = sum(pop)), by = c("country_code", "name", "year", "agecat", "age5")][, agecat := NULL]
-# pop5.1 <- pop5[year %in% seq(1949, 2019, by = 5)]
-# pop5.2 <- pop5[year %in% seq(1950, 2020, by = 5)]
-# pop5 <- pop5.2[, .(country_code, name, year, age = age5, popM = round((pop5.1$popM + pop5.2$popM)/2,3), 
-#                    popF = round((pop5.1$popF + pop5.2$popF)/2,3))][, pop := popM + popF]
-# 
-# popAge5dt <- pop5[]
-
-
 setwd(data.dir)
 
+# 5-year age-specific pop estimates 
 source("popAge5dt.R")
 popAge5dt[, name := NULL]
 use_data(popAge5dt, overwrite = TRUE)
 
+# 5-year projections
 source("popprojAge5dt.R")
 popprojAge5dt[, name := NULL]
 use_data(popprojAge5dt, overwrite = TRUE)
 
+# remove big 1x1 datasets
 files.to.remove <- c("popprojAge1dt.rda", "popprojAge1dt.R", "popAge1dt.rda", "popAge1dt.R", "mx1dt.rda", "mx1dt.R")
 for(f in files.to.remove) unlink(f)
 
+# copy new version of the corresponding R files for the 5x5 pop
 setwd(wrkdir)
-
 files.to.copy <- c("popAge5dt.R", "popprojAge5dt.R")
 file.copy(files.to.copy, data.dir, overwrite = TRUE)
 
+# update datalist
 tools::add_datalist("..", force = TRUE)
